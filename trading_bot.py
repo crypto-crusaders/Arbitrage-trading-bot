eff0aba1a24e4406a970afe4f9470a420dcc80dc
Xander Q <xanderqiang@outlook.com>
2022-10-24 00:50:47 +0900

    Calculate recommedations

--- trading_bot.py
+++ trading_bot.py
@@ -11,17 +11,18 @@ TRADING_TICKER_NAME = 'btcusdt'
 INVESTMENT_AMOUNT_DOLLARS = 10
 HOLDING_QUANTITY = 0
 
+MARGIN = 100
+
 exchange_names =[
-    'binance',
-    'bitfinex',
-    'bittrex',
-    'poloniex',
-    'bitget',
-    'bitmart',
-    'bitvavo',
+    # 'binance',
+    # 'bitfinex',
+    # 'bittrex',
+    # 'poloniex',
+    # 'bitget',
+    # 'bitmart',
+    # 'bitvavo',
     'bitforex',
     'kuna',
-
 ]
 
 exchange_length = len(exchange_names)
@@ -30,8 +31,10 @@ exchanges = []
 
 def debug(term, value=None):
     print(f'<== {term} ==>\n\n', value, '\n')
+
 def console(value):
     print(value)
+
 for i in range(0, exchange_length):
     exchanges.append(getattr(ccxt, exchange_names[i])())
 
@@ -42,9 +45,11 @@ def fetch_data (ticker):
     debug('fetch', ticker)
     data = []
     for i in range(0, exchange_length):
+        debug('exchange id', getattr(exchanges[i], 'id'))
         ticker_data = getattr(exchanges[i], 'fetch_ticker')(ticker)
 
         data.append(ticker_data)
+        debug('ticker', ticker_data)
         # debug('ticker data', ticker_data['last'])
     # ticker = exchanges[0].fetch_ticker(ticker)
     # ticker = getattr(exchanges[0], 'fetch_ticker')(ticker)
@@ -56,16 +61,35 @@ def fetch_data (ticker):
     return data
 
 def get_trade_recommendation(ticker_df):
-    print('get trade recommendation\n\n')
+    if not ticker_df or ticker_df == None or len(ticker_df) == 0:
+        print(f'ticker_df should not None')
+        return None
 
-def execute_trade(trade_rec_type, trading_ticker):
+    max_ticker = min_ticker = ticker_df[0]
+
+    for item in ticker_df:
+        if item['last'] > max_ticker['last']:
+            max_ticker = item
+        elif item['last'] < min_ticker['last']:
+            min_ticker = item
+
+    return max_ticker, min_ticker    
+
+def execute_trade(trade_rec_type, trading_ticker, market):
     debug('execute trade')
 
 def run_bot_for_ticker(ccxt_ticker, trading_ticker):
     debug('run bot')
-    ticker_data = fetch_data(ccxt_ticker)
+    try:
+        
+        ticker_data = fetch_data(ccxt_ticker)
 
-    trade = get_trade_recommendation(ticker_data)
+        ticker_max, ticker_min = get_trade_recommendation(ticker_data)
+
+        if ticker_max['last'] > ticker_min['max'] > MARGIN:
+            execute_trade(ccxt_ticker, )
+    except:
+        print(f'error occurred while trade tokens')
 
 run_bot_for_ticker(CCXT_TICKER_NAME, TRADING_TICKER_NAME)
 
